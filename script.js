a12277bc42680ba46d2e34f5626b910eb2b43304
tessbishop <tess.bishop8@gmail.com>
2020-06-01 10:49:06 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var string = "Baby don't _____ me.";
+
+var missingLyric = "hurt";
+
+var newString = "Baby don't " + missingLyric + " me.";
+
+var song = document.getElementById("song");
+song.innerText = newString;
+song.innerText = "What is love";
+song.innerText = newString; 
